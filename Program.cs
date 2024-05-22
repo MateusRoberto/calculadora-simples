a45337b33d7dc7d34dbd2d889745f488151271be
unknown <mateusroberto2609@gmail.com>
2024-05-22 18:53:34 -0300

    Calculadora

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("-----Calculadora------");
+
+            bool continuar = true;
+
+            while (continuar)
+            {
+
+
+                Console.WriteLine("Digite um número: ");
+                double n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite um número: ");
+                double n2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o Operador: ");
+                string operador = (Console.ReadLine());
+
+                double resultado;
+
+
+
+
+
+
+
+                switch (operador)
+                {
+                    case "+":
+                        resultado = n1 + n2;
+                        Console.WriteLine(resultado);
+                        break;
+
+                    case "-":
+                        resultado = n1 - n2;
+                        Console.WriteLine(resultado);
+                        break;
+
+                    case "*":
+                        resultado = n1 * n2;
+                        Console.WriteLine(resultado);
+                        break;
+
+                    case "/":
+                        resultado = n1 / n2;
+                        Console.WriteLine(resultado);
+                        break;
+
+
+                }
+
+                Console.WriteLine("Deseja Continuar? (Sim/Não)");
+                string resposta = Console.ReadLine();
+
+                if (resposta != "sim" && resposta != "Sim" && resposta != "SIM")
+                {
+                    continuar = false;
+                    Console.WriteLine("Obrigado!");
+                }
+            }
+
+        }
+    }
+}
